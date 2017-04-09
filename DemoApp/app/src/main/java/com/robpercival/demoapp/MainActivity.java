20b6def1c61e49f13797c3853c8c20fe341f92b7
kgit1 <kgit1@rambler.ru>
2017-04-10 00:30:41 +0300

    Toast

--- DemoApp/app/src/main/java/com/robpercival/demoapp/MainActivity.java
+++ DemoApp/app/src/main/java/com/robpercival/demoapp/MainActivity.java
@@ -5,12 +5,26 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     public void clickFunction(View view){
         EditText stringEnterName = (EditText) findViewById(R.id.myTextField);
         EditText stringEnterPassword = (EditText) findViewById(R.id.myPasswordField);
+
+        Toast.makeText(MainActivity.this, "Login: "+stringEnterName.getText().toString(),
+                Toast.LENGTH_SHORT).show();
+
+        Toast.makeText(MainActivity.this, "Password: "+stringEnterPassword.getText().toString(),
+                Toast.LENGTH_SHORT).show();
+
+        Toast.makeText(MainActivity.this, "Hi there, Queen of Meereen, Queen of the Andals" +
+                "(, the Rhoynar) and the First Men, Lady Regnant of the Seven Kingdoms, Khaleesi"+
+                "of the Great Grass Sea, Mhysa, Breaker of Chains, the Unburnt, Mother of Dragons",
+                Toast.LENGTH_LONG).show();
+        
+
         System.out.println("Clicked");
         Log.i("Info", "Button pressed");
         Log.i("Name", stringEnterName.getText().toString());
