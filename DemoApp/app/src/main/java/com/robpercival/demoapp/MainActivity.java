fb88b7d9ebfea7b6209281a9f0df49bd159ff102
kgit1 <kgit1@rambler.ru>
2017-04-10 00:12:43 +0300

    field name password handle

--- DemoApp/app/src/main/java/com/robpercival/demoapp/MainActivity.java
+++ DemoApp/app/src/main/java/com/robpercival/demoapp/MainActivity.java
@@ -4,12 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     public void clickFunction(View view){
+        EditText stringEnterName = (EditText) findViewById(R.id.myTextField);
+        EditText stringEnterPassword = (EditText) findViewById(R.id.myPasswordField);
         System.out.println("Clicked");
         Log.i("Info", "Button pressed");
+        Log.i("Name", stringEnterName.getText().toString());
+        Log.i("Password",stringEnterPassword.getText().toString());
     }
 
     @Override
---
+++ DemoAppTaskButton/app/src/main/java/com/example/a/demoapptaskbutton/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.a.demoapptaskbutton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void clickButton(View view){
+        Log.i("Infoooooooo","Butt button clicked");
+    }
+}
