7f08e47bf522d680ddd7d601b70c3e1580270441
kgit1 <kgit1@rambler.ru>
2017-04-18 04:15:34 +0300

    up translationXY scale etc

--- AppFadeAnimation/app/src/main/java/com/example/a/appfadeanimation/MainActivity.java
+++ AppFadeAnimation/app/src/main/java/com/example/a/appfadeanimation/MainActivity.java
@@ -14,20 +14,31 @@ public class MainActivity extends AppCompatActivity {
 
         ImageView imageBart = (ImageView) findViewById(R.id.imageBart);
         ImageView imageBart2 = (ImageView) findViewById(R.id.imageBart2);
+        // imageBart2.setTranslationY(-2000f);
 
         if (!clicked) {
+            functionRestartImage(imageBart2);
+            imageBart2.setAlpha(0f);
             //animate() needs to know what and how long to do
             //alpha - transparency
             imageBart.animate().alpha(0f).setDuration(2000);
             imageBart2.animate().alpha(1f).setDuration(2000);
             clicked = true;
         } else {
+            functionRestartImage(imageBart2);
             imageBart.animate().alpha(1f).setDuration(2000);
-            imageBart2.animate().alpha(0f).setDuration(2000);
+            //translation moving object on amount of pixels
+            //scale changing images scale 0-1
+            imageBart2.animate().translationYBy(2000f).translationX(1000f).scaleX(0.5f).scaleY(0.5f).setDuration(2000);
             clicked = false;
         }
+    }
 
-
+    private void functionRestartImage(ImageView image) {
+        image.setTranslationY(0f);
+        image.setTranslationX(0f);
+        image.setScaleX(1f);
+        image.setScaleY(1f);
     }
 
     @Override
