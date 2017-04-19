f910f9d6477100447a19f61aa67f9dfa15d06f8a
kgit1 <kgit1@rambler.ru>
2017-04-19 23:47:22 +0300

    up

--- AppSound/app/src/main/java/com/example/a/appsound/MainActivity.java
+++ AppSound/app/src/main/java/com/example/a/appsound/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.media.MediaPlayer;
 import android.view.View;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
     //soundbible.com - to download various sound files
@@ -28,5 +29,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SeekBar volumeControl = (SeekBar) findViewById(R.id.seekBarVolume);
+
+        //listenning when user changing position on seekbar
+        volumeControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
