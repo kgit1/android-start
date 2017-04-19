5d75ae14adb2c1947675d2323c134a339e83a074
kgit1 <kgit1@rambler.ru>
2017-04-19 22:37:22 +0300

    app media sound + buttons

---
+++ AppSound/app/src/main/java/com/example/a/appsound/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.a.appsound;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.media.MediaPlayer;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    //soundbible.com - to download various sound files
+
+    MediaPlayer mediaPlayer;
+
+    public void functionPlayButton(View view){
+        mediaPlayer = MediaPlayer.create(this, R.raw.laugh);
+        mediaPlayer.start();
+    }
+
+    public void functionPauseButton(View view){
+        mediaPlayer.pause();
+    }
+
+    public void functionStopButton(View view){
+        mediaPlayer.stop();
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
