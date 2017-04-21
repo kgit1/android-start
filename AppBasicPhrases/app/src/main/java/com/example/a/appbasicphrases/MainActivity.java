29202353409b1b3682c04acc9abdff7a0dc36cdf
kgit1 <kgit1@rambler.ru>
2017-04-21 04:17:33 +0300

    up

--- AppBasicPhrases/app/src/main/java/com/example/a/appbasicphrases/MainActivity.java
+++ AppBasicPhrases/app/src/main/java/com/example/a/appbasicphrases/MainActivity.java
@@ -13,48 +13,69 @@ public class MainActivity extends AppCompatActivity {
     //ielanguages.com/french3s.html
     MediaPlayer mediaPlayer;
 
-    public void functionButtonPlayPhrases(View view){
+    public void functionButtonPlayPhrases(View view) {
         Button button = (Button) findViewById(view.getId());
-        Log.i("Test","button " + view.getId()+"clicked");
-        Log.i("Test","button - " + button.getText().toString()+" - clicked");
-        if(mediaPlayer==null){
+        view.getResources().getResourceEntryName(view.getId());
+
+       /* //create int to hold view object id
+        int id = view.getId();
+        //create string to hold resource name got by object id
+        String ourId = view.getResources().getResourceEntryName(id);
+        //create int to hold file id
+        //give to method getIdentifier() - file id, folder name, package name
+        int resourceId = getResources().getIdentifier(ourId, "raw", "com.example.a.appbasicphrases");
+        Log.i("Test", "ourId - " + ourId + " - clicked");
+        Log.i("Test", "getResources() - " + getResources() + " - clicked");
+        Log.i("Test", "resourceId - " + resourceId + " - clicked");
+        //create mediaplayer and initialize it with resource id, not in usual way (with resource path(like R.raw.doyouspeakenglish))
+        mediaPlayer = MediaPlayer.create(this, resourceId);
+        mediaPlayer.start();
+
+        Log.i("Test", "button - " + view.getId() + " - clicked");
+        Log.i("Test", "button - " + button.getText().toString() + " - clicked");
+        Log.i("Test", "getResourceEntryName - " + view.getResources().getResourceEntryName(view.getId()) + " - clicked");
+        Log.i("Test", "getResourceTypeName - " + view.getResources().getResourceTypeName(view.getId()) + " - clicked");
+        Log.i("Test", "getResourcePackageName - " + view.getResources().getResourcePackageName(view.getId()) + " - clicked");
+        Log.i("Test", "getResourceName - " + view.getResources().getResourceName(view.getId()) + " - clicked");*/
+
+        if (mediaPlayer == null) {
             mediaPlayer = new MediaPlayer();
         }
-        if(mediaPlayer.isPlaying()){
+        if (mediaPlayer.isPlaying()) {
             mediaPlayer.stop();
         }
 
-        switch(button.getTag().toString()){
+        switch (button.getTag().toString()) {
             case "1":
-                mediaPlayer=MediaPlayer.create(this,R.raw.doyouspeakenglish);
+                mediaPlayer = MediaPlayer.create(this, R.raw.doyouspeakenglish);
                 mediaPlayer.start();
                 break;
             case "2":
-                mediaPlayer=MediaPlayer.create(this,R.raw.goodevening);
+                mediaPlayer = MediaPlayer.create(this, R.raw.goodevening);
                 mediaPlayer.start();
                 break;
             case "3":
-                mediaPlayer=MediaPlayer.create(this,R.raw.hello);
+                mediaPlayer = MediaPlayer.create(this, R.raw.hello);
                 mediaPlayer.start();
                 break;
             case "4":
-                mediaPlayer=MediaPlayer.create(this,R.raw.howareyou);
+                mediaPlayer = MediaPlayer.create(this, R.raw.howareyou);
                 mediaPlayer.start();
                 break;
             case "5":
-                mediaPlayer=MediaPlayer.create(this,R.raw.ilivein);
+                mediaPlayer = MediaPlayer.create(this, R.raw.ilivein);
                 mediaPlayer.start();
                 break;
             case "6":
-                mediaPlayer=MediaPlayer.create(this,R.raw.mynameis);
+                mediaPlayer = MediaPlayer.create(this, R.raw.mynameis);
                 mediaPlayer.start();
                 break;
             case "7":
-                mediaPlayer=MediaPlayer.create(this,R.raw.please);
+                mediaPlayer = MediaPlayer.create(this, R.raw.please);
                 mediaPlayer.start();
                 break;
             case "8":
-                mediaPlayer=MediaPlayer.create(this,R.raw.welcome);
+                mediaPlayer = MediaPlayer.create(this, R.raw.welcome);
                 mediaPlayer.start();
                 break;
         }
@@ -65,7 +86,7 @@ public class MainActivity extends AppCompatActivity {
                                                     mediaPlayer.reset();
                                                     mediaPlayer.release();
                                                     mediaPlayer = null;
-                                                    Log.i("Test","mediaplayer nulled");
+                                                    Log.i("Test", "mediaplayer nulled");
                                                 }
                                             }
         );
@@ -76,6 +97,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mediaPlayer= MediaPlayer.create(this,R.raw.doyouspeakenglish);
+        mediaPlayer = MediaPlayer.create(this, R.raw.doyouspeakenglish);
     }
 }
