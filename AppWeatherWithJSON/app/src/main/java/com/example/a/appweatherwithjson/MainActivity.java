620d512e0e7f3e0ca26a8cb77ea0ae4e0e4c6cab
kgit1 <kgit1@rambler.ru>
2017-04-27 04:55:35 +0300

    AppWeatherWithJSON fin

--- AppWeatherWithJSON/app/src/main/java/com/example/a/appweatherwithjson/MainActivity.java
+++ AppWeatherWithJSON/app/src/main/java/com/example/a/appweatherwithjson/MainActivity.java
@@ -5,6 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -16,11 +20,11 @@ import java.util.concurrent.ExecutionException;
 public class MainActivity extends AppCompatActivity {
 
 
-    private class DownloadPageTask extends AsyncTask<String, Void, String>{
+    private class DownloadPageTask extends AsyncTask<String, Void, String> {
 
         @Override
         protected String doInBackground(String... params) {
-            StringBuilder pageHtml=new StringBuilder();
+            StringBuilder pageHtml = new StringBuilder();
 
             try {
                 URL url = new URL(params[0]);
@@ -31,10 +35,10 @@ public class MainActivity extends AppCompatActivity {
                 InputStreamReader reader = new InputStreamReader(in);
                 int data = reader.read();
 
-                while(data>-1){
+                while (data > -1) {
                     char current = (char) data;
                     pageHtml.append(current);
-                    data=reader.read();
+                    data = reader.read();
                 }
 
             } catch (MalformedURLException e) {
@@ -46,6 +50,36 @@ public class MainActivity extends AppCompatActivity {
             }
             return pageHtml.toString();
         }
+
+        //method which called when doInBackground() finished
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            //create json object from the string
+            try {
+                JSONObject jsonObject = new JSONObject(result);
+
+                Log.i("onPostExecute", " - pageHtml result - " + result);
+                //extract "weather" part from object
+                String weatherInfo = jsonObject.getString("weather");
+                Log.i("onPostExecute", "weatherInfo - " + weatherInfo);
+
+                //turn string into jsonArray
+                JSONArray jsonArray = new JSONArray(weatherInfo);
+
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonPart = jsonArray.getJSONObject(i);
+
+                    Log.i("main", jsonPart.getString("main"));
+                    Log.i("description", jsonPart.getString("description"));
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            //Log.i("onPostExecute", " - pageHtml result - "+result);
+        }
     }
 
     @Override
@@ -53,7 +87,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DownloadPageTask task = new DownloadPageTask();
+        new DownloadPageTask().execute("http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=aabddda8fca6982bdf6299a97f0f0100");
+        /*DownloadPageTask task = new DownloadPageTask();
         String pageHtml="";
         try {
             pageHtml = task.execute("http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=aabddda8fca6982bdf6299a97f0f0100").get();
@@ -64,7 +99,7 @@ public class MainActivity extends AppCompatActivity {
             Log.i("Test", "execution");
             e.printStackTrace();
         }
-        Log.i("Test", pageHtml);
+        Log.i("Test", pageHtml);*/
     }
 }
 
