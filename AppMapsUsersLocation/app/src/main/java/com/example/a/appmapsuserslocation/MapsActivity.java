417167ef05c94b0bfdc1e6027a26d29cfc7d74be
kgit1 <kgit1@rambler.ru>
2017-04-30 04:56:04 +0300

    AppMapsUsersLocation + decode geodata to address

--- AppMapsUsersLocation/app/src/main/java/com/example/a/appmapsuserslocation/MapsActivity.java
+++ AppMapsUsersLocation/app/src/main/java/com/example/a/appmapsuserslocation/MapsActivity.java
@@ -2,6 +2,8 @@ package com.example.a.appmapsuserslocation;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -12,6 +14,7 @@ import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -19,6 +22,13 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -43,13 +53,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
+        lat = 0;
+        lon = 0;
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
     }
-
 
     /**
      * Manipulates the map once available.
@@ -77,6 +88,62 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 LatLng userLoc = new LatLng(lat, lon);
                 mMap.addMarker(new MarkerOptions().position(userLoc).title("User-" + lat + ":lon"));
                 mMap.moveCamera(CameraUpdateFactory.newLatLng(userLoc));
+
+                //to transform latitude and longitude to actual address
+                //A class for handling geocoding and reverse geocoding. Geocoding is the process of
+                // transforming a street address or other description of a location into
+                // a (latitude, longitude) coordinate. Reverse geocoding is the process of
+                // transforming a (latitude, longitude) coordinate into a (partial) address.
+                Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+                try {
+                    List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                    if (addresses != null && addresses.size() > 0) {
+                        Log.i("Address", addresses.get(0).toString());
+                        List<String> address = new ArrayList<>();
+                        Pattern p1 = Pattern.compile("0:\"" + "(.*?)" + "\"");
+                        Pattern p2 = Pattern.compile("countryCode=" + "(.*?)" + ",");
+                        Pattern p3 = Pattern.compile("countryName=" + "(.*?)" + ",");
+                        Pattern p4 = Pattern.compile("postalCode=" + "(.*?)" + ",");
+                        Pattern p5 = Pattern.compile("latitude=" + "(.*?)" + ",");
+                        Pattern p6 = Pattern.compile("longitude=" + "(.*?)" + ",");
+
+                        Matcher match = p1.matcher(addresses.get(0).toString());
+                        while (match.find()) {
+                            address.add("Address: " + match.group(1));
+                        }
+                        match = p2.matcher(addresses.get(0).toString());
+                        while (match.find()) {
+                            address.add("countryCode: " + match.group(1));
+                        }
+                        match = p3.matcher(addresses.get(0).toString());
+                        while (match.find()) {
+                            address.add("countryName: " + match.group(1));
+                        }
+                        match = p4.matcher(addresses.get(0).toString());
+                        while (match.find()) {
+                            address.add("postalCod: " + match.group(1));
+                        }
+                        match = p5.matcher(addresses.get(0).toString());
+                        while (match.find()) {
+                            address.add("latitude=: " + match.group(1));
+                        }
+                        match = p6.matcher(addresses.get(0).toString());
+                        while (match.find()) {
+                            address.add("longitude=: " + match.group(1));
+                        }
+
+                        String forToast = "";
+                        for (int i = 0; i < address.size(); i++) {
+                            forToast += "\n" + address.get(i);
+                        }
+                        Log.i("forToast", forToast);
+
+                        Toast.makeText(getApplicationContext(), forToast, Toast.LENGTH_LONG).show();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
             }
 
             @Override
@@ -109,6 +176,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 //to draw marker on map on start, not only when user moves
                 Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
+                Log.i("lastLoc", lastKnownLocation.toString());
                 lat = lastKnownLocation.getLatitude();
                 lon = lastKnownLocation.getLongitude();
                 LatLng userLoc = new LatLng(lat, lon);
