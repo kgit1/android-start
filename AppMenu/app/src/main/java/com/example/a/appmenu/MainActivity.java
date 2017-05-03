e1766346677d27bb7dac37386aee77023db0a24b
kgit1 <kgit1@rambler.ru>
2017-05-03 03:53:56 +0300

    AppMenu + spinner

---
+++ AppMenu/app/src/main/java/com/example/a/appmenu/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.a.appmenu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        switch (item.getItemId()) {
+            case R.id.settings:
+                Log.i("Menu", "settings selected");
+                return true;
+            case R.id.help:
+                Log.i("Menu", " help selected");
+                return true;
+            default:
+                return false;
+        }
+    }
+}
