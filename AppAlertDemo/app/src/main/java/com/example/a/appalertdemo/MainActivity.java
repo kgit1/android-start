e8d921adf3d6123553ae6df36811d0771790f7df
kgit1 <kgit1@rambler.ru>
2017-05-04 22:37:21 +0300

    AppAlertDemo fin with menu

--- AppAlertDemo/app/src/main/java/com/example/a/appalertdemo/MainActivity.java
+++ AppAlertDemo/app/src/main/java/com/example/a/appalertdemo/MainActivity.java
@@ -1,19 +1,48 @@
 package com.example.a.appalertdemo;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    String language;
+    TextView textLanguage;
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new AlertDialog.Builder(this)
+        sharedPreferences = this.getSharedPreferences("com.example.a.appalertdemo", Context.MODE_PRIVATE);
+        textLanguage = (TextView) findViewById(R.id.textView);
+        textLanguage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setLanguage();
+            }
+        });
+
+        language = sharedPreferences.getString("language", "");
+
+        if (language.equals("")) {
+            setLanguage();
+        } else {
+            textLanguage.setText(language);
+        }
+
+
+        /*new AlertDialog.Builder(this)
                 .setIcon(android.R.drawable.ic_dialog_alert)
                 .setTitle("Are you sure?")
                 .setMessage("Do you definitely want to do this?")
@@ -26,6 +55,52 @@ public class MainActivity extends AppCompatActivity {
                     }
                 })
                 .setNegativeButton("No", null)
-                .show();
+                .show();*/
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.languagemenu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        if (item.getItemId() == R.id.english) {
+            useLanguage("English");
+        } else if (item.getTitle().equals("Spanish")) {
+            useLanguage("Spanish");
+        }
+        return true;
+    }
+
+    public void setLanguage() {
+        new AlertDialog.Builder(this)
+                .setIcon(android.R.drawable.ic_btn_speak_now)
+                .setTitle("Which language do you need?")
+                .setMessage("Choose")
+                .setPositiveButton("English", new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        useLanguage("English");
+                    }
+                })
+                .setNegativeButton("Spanish", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                useLanguage("Spanish");
+                            }
+                        }
+                ).show();
+    }
+
+    public void useLanguage(String language) {
+        textLanguage.setText(language);
+        sharedPreferences.edit().putString("language", language).apply();
     }
 }
