5581cc2ed830782bcaf8455e33d83d7388a2ee11
kgit1 <kgit1@rambler.ru>
2017-05-05 23:52:57 +0300

    AppNotesSaver fin intents, alerts, menu

--- AppNotesSaver/app/src/main/java/com/example/a/appnotessaver/EditTextActivity.java
+++ AppNotesSaver/app/src/main/java/com/example/a/appnotessaver/EditTextActivity.java
@@ -1,26 +1,21 @@
 package com.example.a.appnotessaver;
 
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.annotation.Nullable;
-import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.EditText;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Map;
-import java.util.Set;
 
 public class EditTextActivity extends AppCompatActivity {
 
     private EditText editNote;
+    private int noteNumber;
+    private boolean rewrite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,9 +25,14 @@ public class EditTextActivity extends AppCompatActivity {
         editNote = (EditText) findViewById(R.id.editNote);
 
         Intent intent = getIntent();
-        String extra = intent.getStringExtra("note");
-        editNote.setText(extra);
-        Log.i("extra", "" + extra);
+        noteNumber = intent.getIntExtra("note", -1);
+        if (noteNumber > -1) {
+            editNote.setText(MainActivity.notes.get(noteNumber));
+        }
+        Log.i("noteNumber", "" + noteNumber);
+
+        rewrite = intent.getBooleanExtra("rewrite", true);
+        Log.i("rewrite", "" + rewrite);
 
         /*TextView editEntryView = new TextView(...);
         InputFilter[] filterArray = new InputFilter[1];
@@ -43,61 +43,32 @@ public class EditTextActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         if (!editNote.getText().toString().equals("") && !editNote.getText().toString().equals(" ")) {
-            MainActivity.notes.add(editNote.getText().toString());
-            MainActivity.viewNotes.clear();
-            MainActivity.viewNotes.addAll(MainActivity.notes);
-            Collections.reverse(MainActivity.viewNotes);
-            SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.example.a.appnotessaver", Context.MODE_PRIVATE);
-
-            Log.i("back", "button");
-            try {
-                Log.i("notes", MainActivity.notes.toString());
-                Log.i("viewNotes", MainActivity.viewNotes.toString());
-
-                MainActivity.arrayAdapter.notifyDataSetChanged();
-                sharedPreferences.edit().putString("notes", ObjectSerializer.serialize(MainActivity.notes)).apply();
-            } catch (IOException e) {
-                e.printStackTrace();
+            if (!rewrite) {
+                MainActivity.notes.add(editNote.getText().toString());
+                notesWriter();
+            } else {
+                MainActivity.notes.set(noteNumber, editNote.getText().toString());
+                notesWriter();
             }
         }
-
         super.onBackPressed();
+    }
 
-        /* new AlertDialog.Builder(this)
-                .setIcon(android.R.drawable.alert_dark_frame)
-                .setTitle("Choose action")
-                .setMessage("Save changes or Back to list")
-                .setPositiveButton("Save", new DialogInterface.OnClickListener(){
+    private void notesWriter() {
+        MainActivity.viewNotes.clear();
+        MainActivity.viewNotes.addAll(MainActivity.notes);
+        Collections.reverse(MainActivity.viewNotes);
+        MainActivity.arrayAdapter.notifyDataSetChanged();
 
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        if (!editNote.getText().toString().equals("") && !editNote.getText().toString().equals(" ")) {
-                            MainActivity.notes.add(editNote.getText().toString());
-                            MainActivity.viewNotes.clear();
-                            MainActivity.viewNotes.addAll(MainActivity.notes);
-                            Collections.reverse(MainActivity.viewNotes);
-                            SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.example.a.appnotessaver", Context.MODE_PRIVATE);
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.example.a.appnotessaver", Context.MODE_PRIVATE);
 
-                            Log.i("back", "button");
-                            try {
-                                Log.i("notes",MainActivity.notes.toString());
-                                Log.i("viewNotes",MainActivity.viewNotes.toString());
-
-                                MainActivity.arrayAdapter.notifyDataSetChanged();
-                                sharedPreferences.edit().putString("notes", ObjectSerializer.serialize(MainActivity.notes)).apply();
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }
-                })
-                .setNegativeButton("Back", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        Log.i("Back","Answer");
-
-                    }
-
-                }).show();*/
+        Log.i("back", "button");
+        try {
+            Log.i("notes", MainActivity.notes.toString());
+            Log.i("viewNotes", MainActivity.viewNotes.toString());
+            sharedPreferences.edit().putString("notes", ObjectSerializer.serialize(MainActivity.notes)).apply();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- AppNotesSaver/app/src/main/java/com/example/a/appnotessaver/MainActivity.java
+++ AppNotesSaver/app/src/main/java/com/example/a/appnotessaver/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.a.appnotessaver;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
@@ -23,11 +25,16 @@ public class MainActivity extends AppCompatActivity {
     static ArrayList<String> notes = new ArrayList<>();
     static ArrayAdapter<String> arrayAdapter;
     static ArrayList<String> viewNotes;
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        sharedPreferences = this.getSharedPreferences("com.example.a.appnotessaver", Context.MODE_PRIVATE);
+        //sharedPreferences.edit().clear().apply();
 
         ListView listView = (ListView) findViewById(R.id.list);
 
@@ -35,14 +42,49 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent intent = new Intent(getApplicationContext(), EditTextActivity.class);
-                intent.putExtra("note", ""+(notes.size() - 1 - position));
-                Log.i("intent",""+(notes.size() - 1 - position));
+                intent.putExtra("note", (notes.size() - 1 - position));
+                Log.i("intent", "" + (notes.size() - 1 - position));
                 startActivity(intent);
             }
         });
 
-        SharedPreferences sharedPreferences = this.getSharedPreferences("com.example.a.appnotessaver", Context.MODE_PRIVATE);
-        //sharedPreferences.edit().clear().apply();
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setIcon(android.R.drawable.ic_input_delete)
+                        .setTitle("Choose action")
+                        .setMessage("Do you want to delete this note?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Log.i("which", "" + position);
+                                notes.remove(notes.size() - 1 - position);
+                                try {
+                                    sharedPreferences.edit().putString("notes", ObjectSerializer.serialize(notes)).apply();
+
+                                    viewNotes.clear();
+                                    viewNotes.addAll(notes);
+                                    Collections.reverse(viewNotes);
+                                    arrayAdapter.notifyDataSetChanged();
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Log.i("which", "" + position);
+                            }
+                        })
+                        .show();
+                //put true here to work properly with usual click listener
+                return true;
+            }
+        });
+
         try {
             notes.addAll((ArrayList<String>) ObjectSerializer.deserialize(sharedPreferences.getString("notes", ObjectSerializer.serialize(new ArrayList<String>()))));
             Log.i("shared decode", notes.toString());
@@ -53,9 +95,9 @@ public class MainActivity extends AppCompatActivity {
         viewNotes.addAll(notes);
         Collections.reverse(viewNotes);
 
-        if (viewNotes.size() == 0) {
+        /*if (viewNotes.size() == 0) {
             viewNotes.add("new");
-        }
+        }*/
 
         Log.i("adapter", "invoke");
         arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, viewNotes);
@@ -74,6 +116,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         Intent intent = new Intent(this, EditTextActivity.class);
+        intent.putExtra("rewrite", false);
         startActivity(intent);
         return true;
     }
